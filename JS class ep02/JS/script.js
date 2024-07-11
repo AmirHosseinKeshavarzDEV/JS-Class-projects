8060d8980599a1191899ebde7b65c70a9cb50e26
Amir Hossein keshavarz <hduck129@gmail.com>
2024-07-11 14:59:15 -0700

    JS variables and JS mathematics ,  my firs JS error
    + in first ep at JS variables I learn tree function in variables (var, let, const)
    + in JS errors file I added my first error in JS
    +  Test basic mathematics in JS

---
+++ JS class ep02/JS/script.js
@@ -0,0 +1,25 @@
+// js basics variable ep1
+
+// Variables defined with let can not be redeclared.
+// Variables defined with var can be redeclared.
+
+// additionally sting variable in js
+
+var script1 = "amir ";
+
+var script2 = "hossein";
+
+var script3 = script1 + script2;
+
+console.log(script3);
+
+console.log(typeof script3);
+
+// additionally Number variable in js
+
+var X = "2006";
+
+var X = 2005;
+
+console.log(X);
+// alert(X * Y)
